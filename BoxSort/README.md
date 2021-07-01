8473ceda43456fb19e5a91e6d4aa9e55f2c574dd
luotianqi <luotq@anpro-tech.com>
2021-07-01 20:43:17 +0800

    开发框架完成

---
+++ BoxSort/README.md
@@ -0,0 +1,3 @@
+## 构建说明
+
+务必保证 lib META-INF class目录 plugin.yml 在同一目录下
---
+++ BoxSort/build.bat
@@ -0,0 +1,15 @@
+set pluginName=BoxSortPlugin.jar
+set targetPath=E:\Project\Java\Spigot\Servers\plugins\
+@REM 编译
+javac -encoding UTF-8 -cp .\lib\*.jar -d . .\src\ltq\mc\*.java
+@REM 打包
+jar cvfM %pluginName% .\ltq .\lib .\plugin.yml .\META-INF
+@REM 复制插件到服务端
+copy %pluginName% %targetPath%
+@REM 删除生成的文件
+del %pluginName%
+del /s /q ltq
+rd /s /q ltq
+@REM 启动服务
+cd E:\Project\Java\Spigot\Servers
+java -jar .\spigot-1.16.2.jar

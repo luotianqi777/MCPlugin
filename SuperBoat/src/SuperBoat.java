d364f88a9eda1f003b92c04b1bfd01cb154d6433
luotianqi <luotq@anpro-tech.com>
2021-07-06 10:03:31 +0800

    超级船

--- SuperBoat/src/SuperBoat.java
+++ SuperBoat/src/SuperBoat.java
@@ -1,24 +1,40 @@
-import org.bukkit.Bukkit;
-import org.bukkit.entity.Player;
+import org.bukkit.entity.Boat;
+import org.bukkit.entity.LivingEntity;
+import org.bukkit.entity.Vehicle;
 import org.bukkit.event.EventHandler;
-import org.bukkit.event.block.BlockIgniteEvent;
-import org.bukkit.event.block.BlockIgniteEvent.IgniteCause;
+import org.bukkit.event.vehicle.VehicleDamageEvent;
+import org.bukkit.event.vehicle.VehicleEnterEvent;
+import org.bukkit.potion.PotionEffect;
+import org.bukkit.potion.PotionEffectType;
+import org.bukkit.util.Vector;
 
 public class SuperBoat implements org.bukkit.event.Listener{
 
+	private static void KeepFly(Vehicle boat) {
+		boat.setVelocity(new Vector(0,0.2,0));
+	}
+
+	private static void KeepFire(LivingEntity entity){
+		entity.addPotionEffect(new PotionEffect(PotionEffectType.FIRE_RESISTANCE, 1000, 1));
+	}
+
 	@EventHandler
-	/**
-	 * 监听点燃事件
-	 * @param e 点燃事件
-	 */
-	public void OnFire(BlockIgniteEvent e){
-		// 被岩浆点燃
-		if (e.getCause() == IgniteCause.LAVA){
-			for (Player player : Bukkit.getOnlinePlayers()){
-				player.sendMessage("something be fired by lava!");
-			}
-			// 取消事件
+	public void OnDamage(VehicleDamageEvent e){
+		if (e.getVehicle() instanceof Boat){
 			e.setCancelled(true);
+			KeepFly(e.getVehicle());
 		}
 	}
+
+	@EventHandler
+	public void OnEnter(VehicleEnterEvent e){
+		if (e.getVehicle() instanceof Boat){
+			KeepFly(e.getVehicle());
+			if (e.getEntered() instanceof LivingEntity){
+				// 防火效果
+				KeepFire((LivingEntity)e.getEntered());
+			}
+		}
+	}
+
 }

b87e8944a4e4e877497735692346c92acc9ae48f
luotianqi <luotq@anpro-tech.com>
2021-07-15 12:02:49 +0800

    改用自动生成manifest文件

--- ZangAI/build.bat
+++ ZangAI/build.bat
@@ -3,7 +3,7 @@ set targetPath=E:\Project\Java\Spigot\Servers\plugins\
 @REM 编译
 javac -encoding UTF-8 -cp ..\spigot-1.16.2.jar -d . .\src\*.java
 @REM 打包
-jar cvfM %pluginName% *.class .\plugin.yml .\META-INF
+jar cvf %pluginName% *.class .\plugin.yml
 @REM 复制插件到服务端
 copy %pluginName% %targetPath%
 @REM 删除生成的文件
